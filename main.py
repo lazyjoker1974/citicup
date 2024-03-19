2aca3f7ffc329a6b06720e355c25fa48d5198664
lazyjoker1974 <94609524+lazyjoker1974@users.noreply.github.com>
2024-03-20 01:30:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,3 @@ st.title('简单的Streamlit示例')
 
 # 用户输入框
 user_input = st.text_input('请输入您的名字：')
-
-# 显示用户输入的文本
-st.write('您输入的名字是：', user_input)
-
-# 显示用户输入的文本（使用Markdown格式）
-st.markdown(f'您输入的名字是：**{user_input}**')
